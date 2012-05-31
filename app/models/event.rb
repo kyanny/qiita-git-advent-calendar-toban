a350073da06f68878fdf2212865e006776017b6e
Kensuke Nagae <kyanny@gmail.com>
2012-05-31 16:49:07 +0900

    fix

--- app/models/event.rb
+++ app/models/event.rb
@@ -10,7 +10,7 @@ class Event < ActiveRecord::Base
     open('http://api.atnd.org/events/users/?event_id=29378&format=json'){ |f|
       JSON.parse(f.read)['events'][0]['users'].each_with_index do |user, index|
         event = Event.new
-        ['twitter_id', 'twitter_img', 'username'].each do |key|
+        ['twitter_id', 'twitter_img', 'nickname'].each do |key|
           event.send("#{key}=", user[key])
         end
         event.start_at = start_date.since((index-1).days).beginning_of_day
